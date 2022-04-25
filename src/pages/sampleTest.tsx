c8d2ec8cdf7e56b05b4ccc9ad0e107c5543acfd9
ferranm99 <ferranm99@gmail.com>
2022-04-25 04:09:32 +0300

    testpage

--- src/pages/sampleTest.tsx
+++ src/pages/sampleTest.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HomePage() {
   return (
     <main>
-      <h1>Testing Next.js With Jest and React Testing Library</h1>
+      <h1>Testing Next.js With Jest and React Testing Library. React testing</h1>
     </main>
   );
 }
